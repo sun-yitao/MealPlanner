c00177464d54ad1e9b5348a253d02f7e0f389afc
Sun Yitao <sunyitao7@gmail.com>
2021-08-17 02:39:29 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # MealPlanner
 Script to generate .ics file that contains planned meals for the next N days based on a list of acceptable options
+
+## How to use
+
+1. Install dependencies
+2. Modify acceptable food options in script and num_days to plan ahead
+3. Run and double click .ics file to import to calendar
---
+++ mealplan.py
@@ -0,0 +1,43 @@
+import argparse
+import arrow
+from ics import Calendar, Event
+import random
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Plan your meals.')
+    parser.add_argument('--start-date', type=str, default='17-08-21',
+                        required=False, help='dd-mm-yy')
+    parser.add_argument('--lunch-time', type=str, default='12:30',
+                        required=False, help='hh:mm')
+    parser.add_argument('--dinner-time', type=str, default='18:30',
+                        required=False, help='hh:mm')
+    args = parser.parse_args()
+    acceptable_foods = [
+        'Udon don', 'Hwangs', 'Sapore', 'Dumplings', 'Salad', 'Wok Fried',
+        'Chicken Rice', 'Cai Fan', 'Noodles'
+    ]
+    random.shuffle(acceptable_foods)
+
+    ptr = 0
+    num_days = 7
+    planned_foods = []
+    for _ in range(num_days * 2):
+        planned_foods.append(acceptable_foods[ptr % len(acceptable_foods)])
+        ptr += 1
+
+    c = Calendar()
+    for i, planned_food in enumerate(planned_foods):
+        is_lunch = i % 2 == 0
+        prefix = 'Lunch: ' if is_lunch else 'Dinner: '
+        e = Event()
+        e.name = prefix + planned_food
+        time = args.lunch_time if is_lunch else args.dinner_time
+        date = arrow.get(args.start_date + ' ' + time, 'DD-MM-YY HH:mm',
+                         tzinfo='Asia/Singapore')
+        e.begin = date.shift(days=i//2)
+        e.duration = {'minutes': 30}
+        c.events.add(e)
+
+    with open('planned_foods.ics', 'w') as my_file:
+        my_file.writelines(c)
